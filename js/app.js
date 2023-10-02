b98addf46c26119419a1b73b0b495ba171fb31bf
Sam Pearson <98962009+sampearson-xyz@users.noreply.github.com>
2023-10-02 20:50:32 +0100

    new

---
+++ js/app.js
@@ -0,0 +1,48 @@
+$('.featured-button, #intro .circle').on('click', function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 2000);
+});
+// Timeline Scroll Section
+// --------------------------------------------------------------
+var items = $(".timeline li"),
+    timelineHeight = $(".timeline ul").height(),
+    greyLine = $('.default-line'),
+    lineToDraw = $('.draw-line');
+
+// sets the height that the greyLine (.default-line) should be according to `.timeline ul` height
+
+// run this function only if draw line exists on the page
+if(lineToDraw.length) {
+    $(window).on('scroll', function () {
+
+        // Need to constantly get '.draw-line' height to compare against '.default-line'
+        var redLineHeight = lineToDraw.height(),
+            greyLineHeight = greyLine.height(),
+            windowDistance = $(window).scrollTop(),
+            windowHeight = $(window).height() / 2,
+            timelineDistance = $(".timeline").offset().top;
+
+        if(windowDistance >= timelineDistance - windowHeight) {
+            line = windowDistance - timelineDistance + windowHeight;
+
+            if(line <= greyLineHeight) {
+                lineToDraw.css({
+                    'height' : line + 20 + 'px'
+                });
+            }
+        }
+
+        // This takes care of adding the class in-view to the li:before items
+        var bottom = lineToDraw.offset().top + lineToDraw.outerHeight(true);
+        items.each(function(index){
+            var circlePosition = $(this).offset();
+
+            if(bottom > circlePosition.top) {
+                $(this).addClass('in-view');
+            } else {
+                $(this).removeClass('in-view');
+            }
+        });
+    });
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./js/*.js",
+  ],
+  theme: {
+    fontFamily: {
+      'proximate': ['Proximate', 'sans-serif'],
+      'nunito': ['Nunito', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
